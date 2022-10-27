94c5695ac95b72f6733de7d70a507f0ae8a1050c
Max Lutovinov <90464472+maxlutovinov@users.noreply.github.com>
2022-10-27 15:36:37 +0300

    Implement project

---
+++ src/main/java/cinema/controller/UserController.java
@@ -0,0 +1,31 @@
+package cinema.controller;
+
+import cinema.dto.response.UserResponseDto;
+import cinema.model.User;
+import cinema.service.UserService;
+import cinema.service.mapper.ResponseDtoMapper;
+import java.util.NoSuchElementException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+    private final ResponseDtoMapper<UserResponseDto, User> userResponseDtoMapper;
+
+    public UserController(UserService userService,
+                          ResponseDtoMapper<UserResponseDto, User> userResponseDtoMapper) {
+        this.userService = userService;
+        this.userResponseDtoMapper = userResponseDtoMapper;
+    }
+
+    @GetMapping("/by-email")
+    public UserResponseDto findByEmail(@RequestParam String email) {
+        User user = userService.findByEmail(email).orElseThrow(
+                () -> new NoSuchElementException("User with email " + email + " not found"));
+        return userResponseDtoMapper.mapToDto(user);
+    }
+}
